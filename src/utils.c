90a1ef73bc3ff88cf454af49c766d6bfbc6b863d
= <=>
2025-06-29 09:24:51 +0000

    remove obselete function

--- src/utils.c
+++ src/utils.c
@@ -13,35 +13,3 @@ void log_unidentified_token(int line, char *buffer) {
   fprintf(stderr, "Error: Unidentified token %s at line %d\n%d\n", buffer, line,
           errno);
 }
-
-char *tokentype_get(token *token) {
-  tokenType tk = token->type;
-
-  switch (tk) {
-  case RETURN:
-    return "return";
-  case EOS:
-    return "eos";
-  case IDENTIFIER:
-    return "identifier";
-  case COMMA:
-    return "comma";
-  case RBRACKET:
-    return "rbracket";
-  case LBRACKET:
-    return "lbracket";
-  case INT:
-    return "int";
-  case PLUS:
-    return "plus";
-  case MINUS:
-    return "minus";
-  case IF:
-    return "if";
-  case GOTO:
-    return "goto";
-
-  default:
-    return "undefined";
-  }
-}
--- src/utils.h
+++ src/utils.h
@@ -4,4 +4,3 @@
 
 void log_error(const char *message);
 void log_unidentified_token(int line, char *buffer);
-char *tokentype_get(token *token);
